ed17870628d2bba613499ac0862c77836abe6531
Administrator <Administrator@USER-20150725QL>
2015-10-27 07:32:34 +0800

    update atr

---
+++ atr_market_test.py
@@ -0,0 +1,6 @@
+from tradeStrategy import *
+
+print datetime.datetime.now()
+atr_market()
+back_test_atr()
+print datetime.datetime.now()
---
+++ atr_stock_test.py
@@ -0,0 +1,18 @@
+from tradeStrategy import *
+
+import pandas_talib as ta
+
+code='002678'
+#code='000987'
+#code='601018'
+#code='002466'
+#code='600650'
+#code='300244'
+#code='000001'
+#code='300033'
+short_num=20
+long_num=55
+dif_num=9
+current_price=12.10
+stock=Stockhistory(code,'D')
+df=stock.get_atr_df(short_num, long_num)
