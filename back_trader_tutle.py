1a1100c9eaac8e875b9c85289876f614f05ecab2
Administrator <Administrator@USER-20150725QL>
2015-11-01 23:43:01 +0800

    update ma strategy

--- back_trader_tutle.py
+++ back_trader_tutle.py
@@ -25,7 +25,17 @@ class TestStrategy(bt.Strategy):
         # Simply log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
 """
-class TestStrategy(bt.Strategy):
+class Broker(bt.Observer):
+    alias = ('CashValue',)
+    lines = ('cash', 'value')
+
+    plotinfo = dict(plot=True, subplot=True)
+
+    def next(self):
+        self.lines.cash[0] = self._owner.broker.getcash()
+        self.lines.value[0] = value = self._owner.broker.getvalue()
+        
+class TutleStrategy(bt.Strategy):
     """
     params = (
         ('stake', 4000),
@@ -47,7 +57,7 @@ class TestStrategy(bt.Strategy):
 
     def __init__(self):
         # Keep a reference to the "close" line in the data[0] dataseries
-        print ('datas=%s' % self.datas[0])
+        #print ('datas=%s' % self.datas[0])
         self.dataclose = self.datas[0].close
         self.datahigh = self.datas[0].high
         self.datalow = self.datas[0].low
@@ -71,6 +81,12 @@ class TestStrategy(bt.Strategy):
         self.lowest=bt.indicators.Lowest(self.datas[0],period=20)
         #self.highest=bt.indicators.Highest(self.datahigh,period=20)
         #self.lowest=bt.indicators.Lowest(self.datalow,period=20)
+        #cash=self.stats.broker.cash[0]
+        #value=self.stats.broker.value[0]
+       
+        #print('cash=',cash)
+        #print('value=',value)
+        
         """
         bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
         bt.indicators.WeightedMovingAverage(self.datas[0], period=25,
@@ -161,17 +177,180 @@ class TestStrategy(bt.Strategy):
 
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
+class MaStrategy(bt.Strategy):
+    """
+    params = (
+        ('stake', 4000),
+        ('exitbars', 4),
+    )
+    """
+    params = (
+        ('s_maperiod', 5),
+        ('l_maperiod',10),
+        ('stake', 100),
+        ('exit_point',1.0),
+        ('buy_point_atr',1),
+        ('terminate_profit_factor',18.0),           #1.6-1.8 will be more better
+        ('printlog', False),
+    )
+
+    def log(self, txt, dt=None,doprint=False):
+        ''' Logging function fot this strategy'''
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        #print ('datas=%s' % self.datas[0])
+        self.dataclose = self.datas[0].close
+        self.datahigh = self.datas[0].high
+        self.datalow = self.datas[0].low
+        
+        # Set the sizer stake from the params
+        self.sizer.setsizing(self.params.stake)
+
+        # To keep track of pending orders and buy price/commission
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+        
+        # Add a MovingAverageSimple indicator
+        self.s_sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.s_maperiod)
+        self.l_sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.l_maperiod)
+        self.atr=bt.indicators.ATR(self.datas[0])
+        # Indicators for the plotting show
+        self.highest=bt.indicators.Highest(self.datas[0],period=20)
+        self.lowest=bt.indicators.Lowest(self.datas[0],period=20)
+        #self.highest=bt.indicators.Highest(self.datahigh,period=20)
+        #self.lowest=bt.indicators.Lowest(self.datalow,period=20)
+        #cash=self.stats.broker.cash[0]
+        #value=self.stats.broker.value[0]
+       
+        #print('cash=',cash)
+        #print('value=',value)
+        
+        """
+        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
+        bt.indicators.WeightedMovingAverage(self.datas[0], period=25,
+                                            subplot=True)
+        bt.indicators.StochasticSlow(self.datas[0])
+        bt.indicators.MACDHisto(self.datas[0])
+        rsi = bt.indicators.RSI(self.datas[0])
+        bt.indicators.SmoothedMovingAverage(rsi, period=10)
+        """
+        #bt.indicators.ATR(self.datas[0], subplot=True)#plot=False)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enougth cash
+        if order.status in [order.Completed, order.Canceled, order.Margin]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        # Write down: no pending order
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+        self.log('High, %.2f' % self.datahigh[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+            """
+            # Not yet ... we MIGHT BUY if ...
+            if self.dataclose[0] > 1.01*self.dataclose[-1]: #and self.dataclose[0] > 1.03*self.dataclose[-1]:
+                    # current close less than previous close
+
+                    if self.dataclose[-1] > 1.01*self.dataclose[-2]:
+            """
+            if self.s_sma[-1]<self.l_sma[-1] and self.s_sma[0]>=self.l_sma[0]:
+                        # previous close less than the previous close
+                        
+                        # BUY, BUY, BUY!!! (with default parameters)
+                        self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                        self.log('atr of BUY point, %.2f' % self.atr[0])
+                        #self.params.exit_point=self.dataclose[0]-2.0*self.atr[0]
+                        self.params.buy_point_atr=self.atr[0]
+                        #cash=100000.00
+                        #buy_price=self.highest[0]
+                        #buy_size=0.01*cash/(self.params.buy_point_atr*100*buy_price)
+                        # Keep track of the created order to avoid a 2nd order
+                        #self.order = self.buy(size=buy_size,price=buy_price)
+                        self.order = self.buy()
+
+        else:
+
+            # Already in the market ... we might sell
+            #if len(self) >= (self.bar_executed +  self.params.exitbars):
+            #if self.datalow[0] < self.lowest[0] or  self.dataclose[0] < self.params.exit_point:
+            if (self.s_sma[-1]>=self.l_sma[-1] and self.s_sma[0]<self.l_sma[0]) or self.dataclose[0]>(self.s_sma[0]+0.1*self.params.terminate_profit_factor*self.atr[-1]):#self.params.buy_point_atr):
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+                
+    def stop(self):
+        self.log('(terminate_profit_factor Period %.2f) Ending Value %.2f' %
+                 (self.params.terminate_profit_factor*0.1, self.broker.getvalue()), doprint=True)
 def runstrat():
     args = parse_args()
 
     # Create a cerebro entity
+    #cerebro = bt.Cerebro()  # default kwarg: stdstats=True
     cerebro = bt.Cerebro(stdstats=False)
+    #cerebro = bt.Cerebro(stdstats=True)
+    #cerebro.addobserver(bt.observers.Broker)
+    #cerebro.addobserver(bt.observers.Trades)
+    #cerebro.addobserver(bt.observers.BuySell)
 
     # Add a strategy
-    cerebro.addstrategy(TestStrategy)
-
+    #cerebro.addstrategy(TutleStrategy)
+    
+    #cerebro.addstrategy(MaStrategy)
+    #"""
+    strats = cerebro.optstrategy(
+        MaStrategy,
+        terminate_profit_factor=range(1, 30))
+    #"""
     # Get a pandas dataframe
     datapath = ('E:/work/stockAnalyze/update/002678.csv')
+    #datapath = ('E:/work/stockAnalyze/update/002466.csv')
+    #datapath = ('E:/work/stockAnalyze/update/002189.csv')
+    #datapath = ('E:/work/stockAnalyze/update/600673.csv')
 
     # Simulate the header row isn't there if noheaders requested
     skiprows = 1 if args.noheaders else 0
---
+++ pandas_base.py
@@ -0,0 +1,193 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+import functools
+
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,30,20,40],'CCC' : [100,50,-30,-50]})
+df.index.values
+
+df.sort_index(axis=0, by='CCC', ascending=True)
+
+df= df.set_index("AAA")
+
+def get_df_length(df):
+    s=str(df)
+    l=s.split('\n')
+    return (len(l)-1)
+
+df.ix[df.AAA >= 5,'BBB'] = -1
+df.ix[df.AAA >= 5,['BBB','CCC']] = 555
+
+"""to keep the 'true', and replace the 'false'"""
+df_mask = pd.DataFrame({'AAA' : [True] * 4, 'BBB' : [False] * 4,'CCC' : [True,False] * 2})
+df.where(df_mask,-1000)
+
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+df['logic'] = np.where(df['AAA'] > 5,'high','low')               #new column
+
+dflow = df[df.AAA <= 5]
+dfhigh = df[df.AAA > 5]
+
+
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+len(df.index) 
+
+
+newseries = df.loc[(df['BBB'] < 25) & (df['CCC'] >= -40), 'AAA']        #'loc' ---row,  &: 'and'
+newseries = df.loc[(df['BBB'] > 25) | (df['CCC'] >= -40), 'AAA']        #|:'or'
+
+df.loc[(df['BBB'] > 25) | (df['CCC'] >= 75), 'AAA'] = 0.1           #'=': return a df
+
+
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+aValue = 43.0
+df.ix[(df.CCC-aValue).abs().argsort()]              #return the previous df: sort by '(df.CCC-aValue).abs().argsort()'
+
+
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+Crit1 = df.AAA <= 5.5
+Crit2 = df.BBB == 10.0
+Crit3 = df.CCC > -40.0
+AllCrit = Crit1 & Crit2 & Crit3
+df[AllCrit]
+
+#or:
+CritList = [Crit1,Crit2,Crit3]
+AllCrit = functools.reduce(lambda x,y: x & y, CritList)
+#http://docs.python.org/library/functools.html
+
+
+#functools:
+l=[1,2,3,4,5,6]
+value=functools.reduce(lambda x,y:x+y,l)   #is:(((1+2)+3)+4)+5)+6
+#value=21
+
+
+
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+df[(df.AAA <= 6) & (df.index.isin([0,2,4]))]
+
+
+data = {'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]}
+df = pd.DataFrame(data=data,index=['foo','bar','boo','kar'])
+"""row"""
+df.loc['bar':'kar'] #Label
+df.ix[0:3] #Same as .iloc[0:3]
+df.ix['bar':'kar'] #Same as .loc['bar':'kar']
+
+df2 = pd.DataFrame(data=data,index=[1,2,3,4]); #Note index starts at 1
+df2.iloc[1:3] #Position-oriented
+df2.iloc[1:2] #Position-oriented
+df2.iloc[:,1:3] #Position-oriented
+#row:2,3
+df2.loc[1:3] #Label-oriented
+#row: 1,2,3
+df2.ix[1:3] #General, will mimic loc (label-oriented)
+#row: 1,2,3
+df2.ix[0:3] #General, will mimic iloc (position-oriented), as loc[0:3] would raise
+#row: 1,2,3
+
+"""to get the lest"""
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+df[~((df.AAA <= 6) & (df.index.isin([0,2,4])))]
+
+
+
+#New Columns
+df = pd.DataFrame({'AAA' : [1,2,1,3], 'BBB' : [1,1,2,2], 'CCC' : [2,1,3,1]})
+source_cols = df.columns # or some subset would work too.
+new_cols = [str(x) + "_cat" for x in source_cols]
+categories = {1 : 'Alpha', 2 : 'Beta', 3 : 'Charlie' }
+df[new_cols] = df[source_cols].applymap(categories.get)
+
+
+df = pd.DataFrame({'AAA' : [1,1,1,2,2,2,3,3], 'BBB' : [2,1,3,4,5,1,2,3]})
+df.loc[df.groupby("AAA")["BBB"].idxmin()]
+
+
+
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+
+#average, 0-->column, 1-row
+df.mean(0)
+df.mean(1)
+
+df.max(0)
+df.max(1)
+
+df['AAA'].idxmax()
+df['AAA'].idxmin()
+
+df.min(0)
+df.min(1)
+
+df.idxmin(0)
+df.idxmax(0)
+
+df.sum(0,skipna=False)
+df.sum(1)
+
+#wu pian biaozhun cha
+df.std(0)
+df.std(1)
+
+
+
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+#insert
+
+df.insert(1, 'DDD', df['CCC'])
+
+del df['CCC']
+
+df.drop('DDD', axis=1)
+
+df.drop(['DDD','CCC'], axis=1)
+p1=df.pop('BBB')
+df.insert(2, 'BBB', p1)
+
+df.dtypes
+df['AAA'].astype(float)
+
+t_df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+actual_101_list=[4,6]
+df_101=t_df[t_df.index.isin(actual_101_list)]
+
+
+
+df['time']=pd.to_datetime(df['time'])
+ss=pd.Series(list(df['lastprint']),index=df['time'])
+ss.resample('30Min','max')
+
+
+df = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+df.order('CCC')
+
+
+import numba as nb
+
+import pandas.io.sql as psql
+
+
+from sqlalchemy import create_engine
+engine = create_engine('mysql+mysqldb://user:passwd@localhost/schema')
+df.to_sql('data', engine)
+df.to_sql('data_chunked', engine, chunksize=1000)
+
+pd.read_sql_table('data', engine)
+pd.read_sql_table('data', engine, index_col='id')
+pd.read_sql_table('data', engine, columns=['Col_1', 'Col_2'])
+
+df.to_sql('table', engine, schema='other_schema')
+pd.read_sql_table('table', engine, schema='other_schema')
+
+pd.read_sql_query('SELECT * FROM data', engine)
+pd.read_sql_query("SELECT id, Col_1, Col_2 FROM data WHERE id = 42;", engine)
+
+for chunk in pd.read_sql_query("SELECT * FROM data_chunks", engine, chunksize=5):
+    print(chunk)
+    
+    
+from pandas.io import sql
+sql.execute('SELECT * FROM table_name', engine)
+sql.execute('INSERT INTO table_name VALUES(?, ?, ?)', engine, params=[('id', 1, 12.2, True)])
