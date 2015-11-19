72c8a1e0f3bcc837571af379cad47cda7eff6e51
ezhguox <ezhguox@CN00103287.ericsson.se>
2015-11-19 13:05:07 +0800

    2to3

--- atr_market_test.py
+++ atr_market_test.py
@@ -1,6 +1,6 @@
 from tradeStrategy import *
 
-print datetime.datetime.now()
+print(datetime.datetime.now())
 atr_market()
 back_test_atr()
-print datetime.datetime.now()
+print(datetime.datetime.now())
